29dc299c0924d7a0ca38dda3fd1ce533f9a7e9fc
Botkarr <rehakbotondl@gmail.com>
2026-05-10 17:51:40 +0200

    elozo modositas eltuntetese

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,4 @@
 // You can pass additional config via defineConfig({ vite: { ... } }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({
-    base: '/fm-26/', // Ide a GitHub repository-d neve kerüljön perjelekkel!
-});
+export default defineConfig();
